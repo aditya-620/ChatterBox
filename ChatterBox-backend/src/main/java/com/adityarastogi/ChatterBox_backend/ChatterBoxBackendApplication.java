18e084e951e17437de5130c34a4c2850d862d29e
Aditya <adityarastogi620@gmail.com>
2025-04-05 21:42:52 +0530

    initial backend setup commit

---
+++ ChatterBox-backend/src/main/java/com/adityarastogi/ChatterBox_backend/ChatterBoxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.adityarastogi.ChatterBox_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatterBoxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatterBoxBackendApplication.class, args);
+	}
+
+}
