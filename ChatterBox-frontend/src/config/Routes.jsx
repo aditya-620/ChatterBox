26aa5c4ffbda1f344f03d8df9e25cf0c1ed53ac4
Aditya <adityarastogi620@gmail.com>
2025-04-06 16:31:43 +0530

    created routes

---
+++ ChatterBox-frontend/src/config/Routes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route } from "react-router";
+import App from "../App";
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/chat" element={<h1>This is about chat</h1>} />
+      <Route path="/about" element={<h1>This is about page</h1>} />
+      <Route path="*" element={<h1>404 Page Not Found</h1>} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
